48a986a3a08089fb9f9baba46410f9db138c96c3
Krista Brehm <kristabrehm1@gmail.com>
2019-03-25 15:31:00 -0400

    changed the nav color to green to go with the how to train your dragon theme, added additional directions in the jumbotron

--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -8,6 +8,7 @@ class Jumbo extends Component {
                 <br /><br />
                 <h1>Clicky Game!</h1>
                 <div><h4>Click on an image to earn points, but don't click on any more than once!</h4></div>
+                <div><h4>Once you reach 12 points you win!</h4></div>
             </div>
         );
     }
